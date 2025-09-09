e5f4e9d33c53f8faea1fd57cf4b8d1a4b583b288
Joshua Morrison <joshuamorrison@Joshuas-MacBook-Air.local>
2025-09-08 21:02:06 -0400

    add Amplify auth add UI

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "fullstackamplify7932952f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
